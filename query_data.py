33c81e1d142e982806b7fd9da5e93bb50f6f94be
Elshan Ahmedov <ahmedovelshan@gmail.com>
2025-05-13 17:44:35 +0400

    my code

--- query_data.py
+++ query_data.py
@@ -1,17 +1,19 @@
 import argparse
-# from dataclasses import dataclass
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_ollama import OllamaEmbeddings, OllamaLLM, ChatOllama
-from langchain_openai import ChatOpenAI
+from langchain.vectorstores.chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
-import os
+from langchain_community.llms.ollama import Ollama
+
+from get_embedding_function import get_embedding_function
+
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
+
 {context}
+
 ---
+
 Answer the question based on the above context: {question}
 """
 
@@ -22,29 +24,29 @@ def main():
     parser.add_argument("query_text", type=str, help="The query text.")
     args = parser.parse_args()
     query_text = args.query_text
+    query_rag(query_text)
 
+
+def query_rag(query_text: str):
     # Prepare the DB.
-    embedding_function = OllamaEmbeddings(model="nomic-embed-text")
+    embedding_function = OllamaEmbeddings(model="nomic-embed-text", base_url=os.getenv("OLLAMA_BASE_URL"))
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    # if len(results) == 0 or results[0][1] < 0.7:
-    #     print(f"Unable to find matching results.")
-    #     return
+    results = db.similarity_search_with_score(query_text, k=5)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
+    # print(prompt)
 
-    model = OllamaLLM(model="llama3")
-    # model = ChatOpenAI()
+    model = Ollama(model="mistral")
     response_text = model.invoke(prompt)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
+    return response_text
 
 
 if __name__ == "__main__":
