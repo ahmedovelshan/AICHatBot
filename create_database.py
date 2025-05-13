8c67e1445502bdb0701435a1ee3e3024b3fb8801
Elshan Ahmedov <ahmedovelshan@gmail.com>
2025-05-13 17:37:10 +0400

    My code

--- create_database.py
+++ create_database.py
@@ -1,4 +1,5 @@
-from langchain_community.document_loaders import DirectoryLoader
+# from langchain_community.document_loaders import DirectoryLoader
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
@@ -20,26 +21,30 @@ DATA_PATH = "data/books"
 
 
 def main():
-    generate_data_store()
+    # Check if the database should be cleared (using the --clear flag).
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("✨ Clearing Database")
+        clear_database()
 
-
-def generate_data_store():
+    # Create (or update) the data store.
     documents = load_documents()
-    chunks = split_text(documents)
-    save_to_chroma(chunks)
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
 
 
 def load_documents():
-    loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    loader = PyPDFDirectoryLoader(DATA_PATH)
     documents = loader.load()
     return documents
 
 
-
 def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
+        chunk_size=800,
+        chunk_overlap=80,
         length_function=len,
         add_start_index=True,
     )
@@ -53,18 +58,72 @@ def split_text(documents: list[Document]):
     return chunks
 
 
+    
 
-def save_to_chroma(chunks):
+def add_to_chroma(chunks: list[Document]):
+    # Load the existing database.
+    embeddings = OllamaEmbeddings(model="nomic-embed-text", base_url=os.getenv("OLLAMA_BASE_URL"))
+    db = Chroma.from_documents(chunks, embeddings, persist_directory=CHROMA_PATH)
+    db.persist()
+
+    # Calculate Page IDs.
+    chunks_with_ids = calculate_chunk_ids(chunks)
+
+    # Add or Update the documents.
+    existing_items = db.get(include=[])  # IDs are always included by default
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+    # Only add documents that don't exist in the DB.
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        db.persist()
+    else:
+        print("✅ No new documents to add")
+
+
+
+def calculate_chunk_ids(chunks):
+
+    # This will create IDs like "data/monopoly.pdf:6:2"
+    # Page Source : Page Number : Chunk Index
+
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # If the page ID is the same as the last one, increment the index.
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        # Calculate the chunk ID.
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        # Add it to the page meta-data.
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
+
+
+def clear_database():
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
-    # Use OllamaEmbeddings with LLaMA3 or any locally running model
-    embeddings = OllamaEmbeddings(model="nomic-embed-text", base_url=os.getenv("OLLAMA_BASE_URL"))
 
-    db = Chroma.from_documents(chunks, embeddings, persist_directory=CHROMA_PATH)
-    db.persist()
-    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
-    
 
 if __name__ == "__main__":
     main()
